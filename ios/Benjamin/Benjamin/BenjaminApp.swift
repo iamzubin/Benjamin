4877d604bf433840fcdb7176ee535d4abb38872d
Shreyas Papinwar <spapinwar@gmail.com>
2023-12-09 02:27:05 +0530

    Initial ios app

---
+++ ios/Benjamin/Benjamin/BenjaminApp.swift
@@ -0,0 +1,17 @@
+//
+//  BenjaminApp.swift
+//  Benjamin
+//
+//  Created by Sangeeta Papinwar on 09/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BenjaminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/Benjamin/Benjamin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Benjamin
+//
+//  Created by Sangeeta Papinwar on 09/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
