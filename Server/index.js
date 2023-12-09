6b7d10db52080775d2c26101b238f9faf717bc65
iamzubin <zchoudhary.10@gmail.com>
2023-12-09 22:06:39 +0530

    add stuff

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const { createAccount } = require('./handlers/CreateAccount');
 const { getHashForTxn } = require('./handlers/GetHashForTxn');
 const { home } = require('./handlers/Home');
+const { modifyUser } = require('./controllers/database.controller');
+
+
 require('dotenv').config();
 
 const app = express();
@@ -15,9 +18,13 @@ app.get('/', home);
 app.post('/createAccount', createAccount);
 app.post('/getHashForTxn', getHashForTxn);
 
+
 // total balance, daily gain/loss,
+  // https://portal.1inch.dev/documentation/portfolio/quick-start
 // coin we own, price history [30], current price, daily gain/loss, max supply, fully diluted market cap, ATH, ATL
+
 // transactions of user and coin, 
+  // https://portal.1inch.dev/documentation/history/swagger?method=get&path=%2Fv2.0%2Fhistory%2F%7Baddress%7D%2Fevents - for user trxn history
 // list of 12 coins
 
 // buy sell api calls
